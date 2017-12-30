89f0ea89d89fcf50e867a75f4e924404cc14f8e7
Anders Bjerner <abjerner@skybrud.dk>
2017-12-30 01:20:10 +0100

    Misc

--- SkriftGridEditors/App_Data/Models/all.generated.cs
+++ SkriftGridEditors/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "b0cfc80637f1e779")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 
 // FILE: models.generated.cs
--- SkriftGridEditors/App_Data/Models/models.generated.cs
+++ SkriftGridEditors/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "b0cfc80637f1e779")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
